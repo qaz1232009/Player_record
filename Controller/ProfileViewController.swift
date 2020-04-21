246d2d091801510b9645c2784c89682541b15b91
ddcfv <qaz1232009@gmail.com>
2020-04-21 17:26:31 +0800

    UI Complete

--- Controller/ProfileViewController.swift
+++ Controller/ProfileViewController.swift
@@ -9,11 +9,34 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-
+    //UI Test Value
+    
+    var user_name:String! = "SEELE"
+    var user_rate:String! = "0.00"
+    var user_count:String! = "2"
+    var music:User_Recent!
+    @IBOutlet var name_lab: UILabel!
+    @IBOutlet var rate_lab: UILabel!
+    @IBOutlet var count_lab: UILabel!
+    
+    func savaCoreData(){
+        if let appDelegate = (UIApplication.shared.delegate as? AppDelegate) {
+            music = User_Recent(context: appDelegate.persistentContainer.viewContext)
+            music.name = "B.B.B"
+            music.level = "EXH"
+            music.rank = "S"
+            music.score = "10000000"
+            appDelegate.saveContext()
+        }
+    }
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        super.viewDidLoad()
+        name_lab.text = user_name
+        rate_lab.text = user_rate
+        count_lab.text = user_count
+        self.savaCoreData()
     }
     
 
--- Controller/RecentTableViewController.swift
+++ Controller/RecentTableViewController.swift
@@ -6,42 +6,138 @@
 //  Copyright © 2020 ddcfv. All rights reserved.
 //
 
+import CoreData
 import UIKit
 
-class RecentTableViewController: UITableViewController {
+class RecentTableViewController: UITableViewController,NSFetchedResultsControllerDelegate{
+    
+    static var tbView: UITableView!
+    var player_recent_data:[User_Recent] = []
+    var fetchResultController:NSFetchedResultsController<User_Recent>!
 
+ 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.tableView.reloadData()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        RecentTableViewController.tbView = tableView;
+        let fetchRequest: NSFetchRequest<User_Recent> = User_Recent.fetchRequest()
+        let sortDescriptor = NSSortDescriptor(key: "name", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        if let appDelegate = (UIApplication.shared.delegate as? AppDelegate) {
+            
+            let context = appDelegate.persistentContainer.viewContext
+            fetchResultController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+            fetchResultController.delegate = self
+            
+            do {
+                try fetchResultController.performFetch()
+                if let fetchedObjects = fetchResultController.fetchedObjects {
+                    player_recent_data = fetchedObjects
+                }
+            } catch {
+                print(error)
+            }
+        }
+        //print(player_recent_data)
     }
 
     // MARK: - Table view data source
 
+    //num of data
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        if player_recent_data.count > 0 {
+            tableView.backgroundView?.isHidden = true
+            tableView.separatorStyle = .singleLine
+        } else {
+            tableView.backgroundView?.isHidden = false
+            tableView.separatorStyle = .none
+        }
+        return 1
+    }
+    
+    //cell controll
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath:
+    IndexPath) -> UITableViewCell {
+        let cellIdentifier = "Recent_cell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! RecentTableViewCell
+        
+        cell.music_name_Label.text = player_recent_data[indexPath.row].name!
+        cell.score_Label.text = player_recent_data[indexPath.row].score!
+        cell.rank_Label.text = player_recent_data[indexPath.row].rank!
+        cell.level_Label.text = player_recent_data[indexPath.row].level!
+        return cell
+        // #warning Incomplete implementation, return the number of rows
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return player_recent_data.count
     }
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+    //action set
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { (action, sourceView, completionHandler) in
+            // Delete the row from the data store
+            if let appDelegate = (UIApplication.shared.delegate as? AppDelegate) {
+                let context = appDelegate.persistentContainer.viewContext
+                let resultToDelete = self.fetchResultController.object(at: indexPath)
+                context.delete(resultToDelete)
+                appDelegate.saveContext()
+                print("delete")
+            }
+            
+            // Call completion handler with true to indicate
+            completionHandler(true)
+        }
+        
+        
+        // Customize the action buttons
+        deleteAction.backgroundColor = UIColor(red: 231.0/255.0, green: 76.0/255.0, blue: 60.0/255.0, alpha: 1.0)
+        deleteAction.image = UIImage(named: "delete")
+        let swipeConfiguration = UISwipeActionsConfiguration(actions: [deleteAction])
+        
+        return swipeConfiguration
     }
-    */
-
+    
+    //update table view
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch type {
+        case .insert:
+            if let newIndexPath = newIndexPath {
+                tableView.insertRows(at: [newIndexPath], with: .fade)
+            }
+        case .delete:
+            if let indexPath = indexPath {
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+        case .update:
+            if let indexPath = indexPath {
+                tableView.reloadRows(at: [indexPath], with: .fade)
+            }
+        default:
+            tableView.reloadData()
+        }
+        
+        if let fetchedObjects = controller.fetchedObjects {
+            player_recent_data = fetchedObjects as! [User_Recent]
+        }
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+          tableView.endUpdates()
+      }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
